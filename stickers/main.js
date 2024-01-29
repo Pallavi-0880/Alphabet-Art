16e39d50fc9cd65f11482ff42b63fc4d833bee46
Pallavi Thakur <pallavithakur0880@gmail.com>
2024-01-29 11:28:44 +0530

    Sent the sticker quantity value to the form page.

--- stickers/main.js
+++ stickers/main.js
@@ -1,4 +1,21 @@
-function myFunction (imge){
+let decrement = document.getElementById("decrement")
+let increment = document.getElementById("increment")
+let quantityInput = document.querySelector(".quantity-input")
+let proceedButton = document.querySelector(".pushable-button")
+
+decrement.addEventListener("click", () => {
+    quantityInput.stepUp(-1)
+})
+increment.addEventListener("click", () => {
+    quantityInput.stepUp(1)
+
+})
+proceedButton.addEventListener("click", () => {
+    const url = `http://127.0.0.1:5500/Alphabet-Art/stickers/buy.html?q=${quantityInput.value}`
+    window.location.replace(url)
+})
+
+function myFunction(imge) {
     let changeImg = document.getElementById("images")
     changeImg.src = imge.src
 }
